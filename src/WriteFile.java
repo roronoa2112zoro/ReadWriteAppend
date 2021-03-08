8016efbb0a871bbe057fece0b9c419adc14df890
Munshi Sahil Aziz <48142096+roronoa2112zoro@users.noreply.github.com>
2021-03-08 10:42:47 +0530

    Update WriteFile.java

--- src/WriteFile.java
+++ src/WriteFile.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class WriteFile {
     public static void main(String[] args){
         try{
-            FileWriter writer = new FileWriter("Myfile.txt", true);
+            FileWriter writer = new FileWriter("MyFile.txt", true);
             writer.write("Hi, this is my first text in this file!!!");
             writer.write("\r\n");
             writer.write("I will be adding my views in this particular file");
